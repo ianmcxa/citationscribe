a57f1df16e7be43beeab3234f190fe1de9077871
Ian Moore <ipmoore@ncsu.edu>
2015-03-29 14:27:57 -0400

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+//jquery scripts for citationscribe
+//= require jquery
+$( document).ready(function() {
+		$('#apa').click(function( event) {
+				$('#apa').addClass('button-primary');
+		});
+
+});
