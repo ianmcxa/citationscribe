ff0e6074fa35f58757bcb5213248303b56865f16
Ian Moore <ipmoore@ncsu.edu>
2015-03-30 01:18:36 -0400

    Lots of new things. Mainly the UI at this point.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,147 @@
 //jquery scripts for citationscribe
 //= require jquery
+
+var showResources, showInput, format, resource, authors;
+
+authors = '<div class="row hiddenAuthor" style="display:none;"><div class="two columns"><label class="u-pull-right">Author</label></div><div class="three columns"><input placeholder="Last Name" type="text" class="u-full-width AuthorLast"></div><div class="two columns"><input placeholder="Initial" type="text" class="u-full-width AuthorInitial"></div><div class="three columns"><input placeholder="First Name" type="text" class="u-full-width AuthorFirst"></div></div>';
+
+showResources = function() {
+	$('#resourceHeader').show('fast');
+	$('#resources').show('fast');
+};
+showInput = function() {
+	if (resource == 'book') {
+			$('#bookInput').show('slow');
+
+			$('#magInput').hide();
+			$('#newsInput').hide();
+			$('#webInput').hide();
+			$('#journalInput').hide();
+			$('#movieInput').hide();
+	} else if(resource == 'magazine') {
+			$('#magInput').show('slow');
+
+			$('#bookInput').hide();
+			$('#newsInput').hide();
+			$('#webInput').hide();
+			$('#journalInput').hide();
+			$('#movieInput').hide();
+	};
+};
+
 $( document).ready(function() {
 		$('#apa').click(function( event) {
-				$('#apa').addClass('button-primary');
+			$('#apa').addClass('button-primary');
+			format = 'apa';
+			showResources();
+
+			$('#mla').removeClass('button-primary');
+			$('#chi').removeClass('button-primary');
+			$('#csc').removeClass('button-primary');
+		});
+		$('#mla').click(function( event) {
+			$('#mla').addClass('button-primary');
+			format = 'mla';
+			showResources();
+
+			$('#apa').removeClass('button-primary');
+			$('#chi').removeClass('button-primary');
+			$('#csc').removeClass('button-primary');
+		});
+		$('#chi').click(function( event) {
+			$('#chi').addClass('button-primary');
+			format = 'chi';
+			showResources();
+
+			$('#mla').removeClass('button-primary');
+			$('#apa').removeClass('button-primary');
+			$('#csc').removeClass('button-primary');
+		});
+		$("#csc").click(function( event) {
+			$('#csc').addClass('button-primary');
+			format = 'csc';
+			showResources();
+
+			$('#mla').removeClass('button-primary');
+			$('#chi').removeClass('button-primary');
+			$('#apa').removeClass('button-primary');
 		});
 
+		//resource buttons
+		$("#book").click(function( event) {
+			$('#book').addClass('button-primary');
+			resource = 'book';
+			showInput();
+
+			$('#magazine').removeClass('button-primary');
+			$('#news').removeClass('button-primary');
+			$('#web').removeClass('button-primary');
+			$('#journal').removeClass('button-primary');
+			$('#movie').removeClass('button-primary');
+		});
+		$("#magazine").click(function( event) {
+			$('#magazine').addClass('button-primary');
+			resource = 'magazine';
+			showInput();
+
+			$('#book').removeClass('button-primary');
+			$('#news').removeClass('button-primary');
+			$('#web').removeClass('button-primary');
+			$('#journal').removeClass('button-primary');
+			$('#movie').removeClass('button-primary');
+		});
+		$("#news").click(function( event) {
+			$('#news').addClass('button-primary');
+			resource = 'news';
+			showInput();
+
+			$('#magazine').removeClass('button-primary');
+			$('#book').removeClass('button-primary');
+			$('#web').removeClass('button-primary');
+			$('#journal').removeClass('button-primary');
+			$('#movie').removeClass('button-primary');
+		});
+		$("#web").click(function( event) {
+			$('#web').addClass('button-primary');
+			resource = 'web';
+			showInput();
+
+			$('#magazine').removeClass('button-primary');
+			$('#book').removeClass('button-primary');
+			$('#book').removeClass('button-primary');
+			$('#journal').removeClass('button-primary');
+			$('#movie').removeClass('button-primary');
+		});
+		$("#journal").click(function( event) {
+			$('#journal').addClass('button-primary');
+			resource = 'journal';
+			showInput();
+
+			$('#magazine').removeClass('button-primary');
+			$('#news').removeClass('button-primary');
+			$('#web').removeClass('button-primary');
+			$('#book').removeClass('button-primary');
+			$('#movie').removeClass('button-primary');
+		});
+		$("#movie").click(function( event) {
+			$('#movie').addClass('button-primary');
+			resource = 'movie';
+			showInput();
+
+			$('#magazine').removeClass('button-primary');
+			$('#news').removeClass('button-primary');
+			$('#web').removeClass('button-primary');
+			$('#journal').removeClass('button-primary');
+			$('#book').removeClass('button-primary');
+		});
+
+		//add author buttons
+		$('#bookAddAuthor').click(function( event) {
+				$('#bookAuthors').append(authors);
+				$('.hiddenAuthor').slideDown();
+		});
+		$('#magAddAuthor').click(function( event) {
+				$('#magAuthors').append(authors);
+				$('.hiddenAuthor').slideDown();
+		});
 });
