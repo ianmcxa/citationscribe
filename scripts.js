834dc811af4ee6e25ca799a5c6ada41d294c87d4
Ian Moore <ipmoore@ncsu.edu>
2015-04-17 10:23:33 -0400

    Minor changes and better styling.

--- scripts.js
+++ scripts.js
@@ -2,9 +2,15 @@
 //= require jquery
 
 var showResources, showInput, format, resource, authors, makeBook, makeMagazine, makeNews, makeWeb, makeJournal, makeMovie, apaBook, mlaBook, chiBook, cseBook;
-authors = '<div class="row hiddenAuthor" style="display:none;"><div class="two columns"><label class="u-pull-right">Author</label></div><div class="three columns"><input placeholder="Last Name" type="text" class="u-full-width AuthorLast"></div><div class="two columns"><input placeholder="Initial" type="text" class="u-full-width AuthorInitial"></div><div class="three columns"><input placeholder="First Name" type="text" class="u-full-width AuthorFirst"></div></div>';
-directors = '<div class="row hiddenAuthor" style="display:none;"><div class="two columns"><label class="u-pull-right">Director</label></div><div class="three columns"><input placeholder="Last Name" type="text" class="u-full-width AuthorLast"></div><div class="two columns"><input placeholder="Initial" type="text" class="u-full-width AuthorInitial"></div><div class="three columns"><input placeholder="First Name" type="text" class="u-full-width AuthorFirst"></div></div>';
 
+authors = ['<div class="row hiddenAuthor" style="display:none;"><div class="two columns"><label class="u-pull-right">Author</label></div><div class="three columns"><input ', ' placeholder="Last Name" type="text" class="u-full-width AuthorLast"></div><div class="two columns"><input ', ' placeholder="Initial" type="text" class="u-full-width AuthorInitial"></div><div class="three columns"><input ', ' placeholder="First Name" type="text" class="u-full-width AuthorFirst"></div></div>'];
+directors = '<div class="row hiddenAuthor" style="display:none;"><div class="two columns"><label class="u-pull-right">Director</label></div><div class="three columns"><input placeholder="Last Name" type="text" class="u-full-width AuthorLast"></div><div class="two columns"><input placeholder="Initial" type="text" class="u-full-width AuthorInitial"></div><div class="three columns"><input placeholder="First Name" type="text" class="u-full-width AuthorFirst"></div></div>';
+var bookAuthors = 1;
+var magAuthors = 1;
+var newsAuthors = 1;
+var webAuthors = 1;
+var jouAuthors = 1;
+var movAuthors = 1;
 
 showResources = function() {
 	$('#resourceHeader').show('fast');
@@ -18,22 +24,9 @@ makeBook = function() {
 	var publisher = $('#bookPublisher').val();
 	var locate = $('#bookLocation').val();
 	var year = $('#bookYear').val();
-	//iterate through and get an array of the authors
-	//This should be done in the format methods. The author issue is solved by formatting at the same time as reading. Which is how it all should be done.
-	var authorFirst = [];
-	$('.AuthorFirst').each(function(i, obj) {
-			authorFirst.push(obj.val());
-	});
-	var authorInitial = [];
-	$('.AuthorInitial').each(function(i, obj) {
-			authorInitial.push(obj.val());
-	});
-	var authorLast = [];
-	$('.AuthorLast').each(function(i, obj) {
-			authorLast.push(obj.val());
-	});
+	var authorArray = $('#bookAuthors').serializeArray();
 	if (format == 'apa') {
-		apaBook(title, authorLast, authorInitial, authorFirst, edition, volume, publisher, locate);
+		apaBook(title, authorArray, edition, volume, publisher, locate);
 	} else if (format == 'mla') {
 		mlaBook();
 	} else if (format == 'chi') {
@@ -42,8 +35,11 @@ makeBook = function() {
 		cseBook();
 	};
 };
-apaBook = function(title, authorLast, authorInitial, authorFirst, edition, volume, publisher, locate) {
-	var citation = '';	
+apaBook = function(title, authorArray, edition, volume, publisher, locate) {
+	var citation = '';
+};
+cseBook = function(title, authorArray, edition, volume, publisher, locate) {
+    var citation = '';
 };
 
 showInput = function() {
@@ -206,28 +202,35 @@ $( document).ready(function() {
 
 		//add author buttons
 		$('#bookAddAuthor').click(function( event) {
-				$('#bookAuthors').append(authors);
+				$('#bookAuthors').append(authors[0] + 'bookAuthorLast' + bookAuthors + authors[1] + 'bookAuthorInitial' + 
+                        bookAuthors + authors[2] + 'bookAuthorFirst' + bookAuthors + authors[3]);
 				$('.hiddenAuthor').slideDown();
+				bookAuthors++;
 		});
 		$('#magAddAuthor').click(function( event) {
 				$('#magAuthors').append(authors);
 				$('.hiddenAuthor').slideDown();
+				magAuthors++;
 		});
 		$('#newsAddAuthor').click(function( event) {
 				$('#newsAuthors').append(authors);
 				$('.hiddenAuthor').slideDown();
+				newsAuthors++;
 		});
 		$('#webAddAuthor').click(function( event) {
 				$('#webAuthors').append(authors);
 				$('.hiddenAuthor').slideDown();
+				webAuthors++;
 		});
 		$('#jouAddAuthor').click(function( event) {
 				$('#jouAuthors').append(authors);
 				$('.hiddenAuthor').slideDown();
+				jouAuthors++;
 		});
 		$('#movieAddAuthor').click(function( event) {
 				$('#movieAuthors').append(directors);
 				$('.hiddenAuthor').slideDown();
+				movAuthors++;
 		});
 
 		//journal source buttons
@@ -243,4 +246,23 @@ $( document).ready(function() {
 				$('#online').slideUp('fast');
 				$('#database').slideDown();
 		});
+        $('#citeBook').click(function( event) {
+                var title = $('#bookTitle').val();
+	            var volume = $('#bookVolume').val();
+	            var edition = $('#bookEdition').val();
+	            var pages = $('#bookPages').val();
+	            var publisher = $('#bookPublisher').val();
+	            var locate = $('#bookLocation').val();
+	            var year = $('#bookYear').val();
+	            var authorArray = $('#bookAuthors').serializeArray();
+	            if (format == 'apa') {
+		            apaBook(title, authorArray, edition, volume, publisher, locate);
+	            } else if (format == 'mla') {
+		            mlaBook();
+	            } else if (format == 'chi') {
+		            chiBook();
+	            } else if (format == 'cse') {
+		            cseBook();
+	            };
+        });
 });
